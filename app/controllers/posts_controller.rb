ad994d8b4777ea09c463a00d4d704b323386564c
sristikashakya123@gmail.com <sristikashakya123@gmail.com>
2017-05-19 13:48:47 +0545

    creating models and controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+	before_action :is_owner?, only: [:edit, :update, :delete]
+	def new
+		@post = Post.new
+	end
+
+	def create
+	  @post = current_user.posts.create(post_params)
+      if @post.valid?
+      	redirect_to root_path
+      else
+      render :new, status: :unprocessable_entity
+      end
+    end
+
+    def show
+    	@post = Post.find(params[:id])
+    end
+
+    def index
+    	@posts = Post.all.order('created_at DESC')
+    end
+
+    def edit
+    	@post = Post.find(params[:id])
+    end
+
+    def update
+	  @post = Post.find(params[:id])
+	  @post.update(post_params)
+  	  if @post.valid?
+    	redirect_to root_path
+  	  else
+      	render :edit, status: :unprocessable_entity
+      end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
+    private
+		def is_owner?
+		  redirect_to root_path if Post.find(params[:id]).user != current_user
+		end
+		def post_params
+			params.require(:post).permit(:user_id, :photo, :description)
+		end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	belongs_to :user
+	mount_uploader :photo, PhotoUploader
+	validates :photo, :description, :user_id, presence: true
 end
